4b5bcd64153bd1e60f6ca1730e7edc3540cb8e30
SteveFoco <sjfoco@svsu.edu>
2014-10-29 12:36:18 -0400

    Started Panel Design

--- CSMS/src/CSSCS421/CSMS.java
+++ CSMS/src/CSSCS421/CSMS.java
@@ -6,17 +6,53 @@
 
 package CSSCS421;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
 /**
  *
  * @author steev_000
  */
 public class CSMS {
 
+  JFrame mainFrame;
+  CardLayout cards;
+  JPanel cardPanel;
+  
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    EventQueue.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new CSMS();
+      }
+    });
   }
   
+  public CSMS() {
+    mainFrame = new JFrame();
+    
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mainFrame.setTitle("Course Scheduling Management System");
+    mainFrame.setSize(500,400);
+    
+    mainFrame.setLocationRelativeTo(null);
+    mainFrame.setLayout(new BorderLayout());
+    
+    Border outline = BorderFactory.createLineBorder(Color.black);
+    
+    
+  }
 }
